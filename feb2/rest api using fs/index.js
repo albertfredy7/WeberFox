34534021cb8568102f178cb1c23ee584d006e943
albertfredy7 <albertfredy007@gmail.com>
2024-02-02 11:31:52 +0530

    done rest api using the file system using express,node.js and file system module

---
+++ feb2/rest api using fs/index.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const app = express()
+const port  = 5000
+
+const fs = require('fs')
+
+
+const users = require('./MOCK_DATA.json')
+
+
+app.use(express.json())
+
+//middleware for encoded form data
+app.use(express.urlencoded({extended:false}))
+
+
+
+// routes
+
+// 1. api end point which gives the all the users data
+
+
+app.get('/api/users',(req,res)=>{
+    return res.json(users)
+})
+
+
+
+// 2. for html rendering routes for display all the first name of the users in the browser
+app.get('/users',(req,res)=>{
+    const html= `
+    <ul>
+        ${users.map(item=>(`
+            <li>${item.first_name}</li>`
+        )).join("")}
+    </ul>
+    `
+    res.send(html)
+})
+
+
+// 3. api endpoint for getting specific users 
+app.get('/api/users/:id',(req,res)=>{
+    const id =Number(req.params.id)
+    const user = users.find(user=>user.id === id)
+    return res.json(user)
+})
+
+// 4. api end point for creating a new user
+
+app.post('/api/users',(req,res)=>{
+    console.log('inside post route');
+    const body = req.body
+    console.log(body);
+    users.push({...body,id:users.length+1})
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        return res.json({status: 'success'} )
+    })
+    
+})
+
+// 5. api endpoint for updating a user
+app.put('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const updatedUser = req.body;
+
+    // Find the index of the user with the specified id
+    const userIndex = users.findIndex(user => user.id === id);
+
+        // Update the user data 
+        users[userIndex] = { ...users[userIndex], ...updatedUser };
+
+        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data) => {
+            return res.json({ status: 'success', message: 'User updated successfully' });
+        });
+    
+});
+
+// 5. api endpoint for deleting a user
+
+app.delete('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex(user => user.id === id);
+
+    // Remove the user from the users array
+    users.splice(userIndex, 1);
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err,data) => {
+        return res.json({ status: 'success', message: 'User deleted successfully' });
+    });
+});
+
+
+
+
+
+// server starting
+app.listen(port,()=>{
+    console.log(`server listening on port ${port}`);
+})
